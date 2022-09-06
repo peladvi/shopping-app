6debfdf0945c81b5982488b16b278821349eba00
peladvi <99794434+peladvi@users.noreply.github.com>
2022-09-06 12:13:07 +0200

    Initial commit

---
+++ src/component/ProductCard/ProductCard.component.js
@@ -0,0 +1,9 @@
+import { PureComponent } from "react";
+
+export class ProductCard extends PureComponent {
+  render() {
+    return <p>We have overridden the ProductCard component!</p>;
+  }
+}
+
+export default ProductCard;
